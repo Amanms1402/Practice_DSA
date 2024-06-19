9df8b524df9d3d36d88eef8577efe0fa79094c79
Aman Kumar Singh <amanms1402@gmail.com>
2024-06-19 20:19:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find maximum volume of a cuboid/find-maximum-volume-of-a-cuboid.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+import math
+
+class Solution:
+    def maxVolume(self, perimeter, area):
+        # Calculate length l using the formula derived from perimeter and area
+        l = (perimeter - math.sqrt(perimeter * perimeter - 24 * area)) / 12
+        
+        # Calculate height h using the formula for height in terms of l
+        h = (perimeter / 4) - (2 * l)
+        
+        # Calculate volume V using the formulas for length, height, and width (l, l, h)
+        V = l * l * h
+        
+        return V
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        perimeter, area = [int(x) for x in input().split()]
+
+        ob = Solution()
+        print('%.2f' % ob.maxVolume(perimeter, area))
+
+# } Driver Code Ends
